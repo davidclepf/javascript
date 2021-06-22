111529c35694d747954135283836a7879b111d1b
David Clepf <davidclepf@gmail.com>
2021-06-22 19:25:37 -0300

    Adicionado novos tópicos e o primeiro projeto: 'app_calculadora'

---
+++ curso-DWC-udemy/projetos/app_calculadora/calculadora.js
@@ -0,0 +1,34 @@
+// criando a função 'calcular()' que receberá dois parâmetros: 'tipo' e 'valor' 
+function calcular(tipo, valor) {
+
+    // se o 'tipo' do botão é uma 'acao'
+    if(tipo === 'acao') {
+
+        // se o 'valor' é idêntico a 'c'
+        if(valor === 'c') {
+            // limpar o visor (id 'resultado')
+            document.getElementById('resultado').value = '';
+        }
+
+        // concatenando a entrada de dados e exibindo no visor
+        if(valor === ' + ' || valor === ' - ' || valor === ' * ' || valor === ' / ' || valor === '.') {
+            document.getElementById('resultado').value += valor;
+        }
+        
+        if(valor === '=') {
+            // atribuindo o valor do campo 'resultado' à uma variável
+            // usando a função 'evlal()' que converterá a string recebida no campo 'resultado' em operação matemática. assim teremos o resultado do cálculo matemático ao invés de uma concatenação
+            var valor_campo = eval(document.getElementById('resultado').value);
+            
+            // substituindo o valor de 'resultado' pelo valor de 'valor_campo' fazendo com que assim o resultado matemático seja exibido no visor
+            document.getElementById('resultado').value = valor_campo
+        }
+
+    // se o tipo for idêntico à 'valor' (atribuido aos números)
+    } else if(tipo === 'valor'){
+        // os valores recebidos no campo com id 'resultado' serão concatenados (os dados recebidos são recebidos como string e serão concatenados quando atribuido o '+')
+        document.getElementById('resultado').value += valor;
+    }
+
+
+}
